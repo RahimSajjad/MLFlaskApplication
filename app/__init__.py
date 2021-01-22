29e0d7ab388c16856c0de7e9c47acb15286f545b
MD ABDUL RAHIM <74109383+RahimSajjad@users.noreply.github.com>
2021-01-22 14:28:57 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,14 @@ app.config['SECRET_KEY']='e6384f8e584848dde42c1ea66df065d0'
 
 ############################# Database ######################################
 ###sqlite:set configaration:site.db should be create in model directory alongside in python module
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finalversion_22.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finalversion_22.db'
 
 ##Xampp database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/mlprojectdb'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rahimsajjad@sqldatabaseforapp:dummy_PW.8642@sqldatabaseforapp.mysql.database.azure.com/mlprojectdb'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rahim@servernameishope:dummy_PW.8642@servernameishope.mysql.database.azure.com/mluserpost'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rahim@rahimsajjad:dummy_PW.8642@rahimsajjad.mysql.database.azure.com/mlprojectbd'
 
 
 ###Create SQLALCHEMY database instance
